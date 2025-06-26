61fe6be1df10249e7cd82c38f28b9fa020e79d43
Isayus <isayusmarin@gmail.com>
2025-06-26 20:48:43 +0200

    Completo el test #19

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,20 +7,18 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
-  this.timeout(5000);
   suite('Integration tests with chai-http', function () {
     // #1
-test('Test GET /hello with no name', function(done) {
-  chai
-    .request(server)
-    .keepOpen()
-    .get('/hello')
-    .end(function(err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.text, 'hello Guest');
-      done();
-    });
-});
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done();
+        });
     });
     // #2
     test('Test GET /hello with your name', function (done) {
@@ -29,18 +27,18 @@ test('Test GET /hello with no name', function(done) {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('Send {surname: "Colombo"} in a PUT request', function (done) {
       chai
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        
         .end(function (err, res) {
           assert.fail();
 
@@ -48,7 +46,7 @@ test('Test GET /hello with no name', function(done) {
         });
     });
     // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
+    test('Send {surname: "da Verrazzano"} in a PUT request', function (done) {
       assert.fail();
 
       done();
@@ -59,17 +57,13 @@ test('Test GET /hello with no name', function(done) {
 const Browser = require('zombie');
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
+  test('Headless browser navigation to the homepage', function (done) {
+    assert.fail();
 
-
-
-  suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
-    });
+    done();
   });
 
-  suite('"Famous Italian Explorers" form', function () {
+  suite('Famous Italian Explorers', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       assert.fail();
